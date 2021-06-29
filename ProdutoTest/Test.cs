11905c63d424226552c5a5c02b45f420756cb907
marianatmarinho <mariana.tmarinho@gmail.com>
2021-06-28 22:16:59 -0300

    Add novos testes para produto

--- ProdutoTest/Test.cs
+++ ProdutoTest/Test.cs
@@ -15,17 +15,49 @@ namespace ProdutoTest
         [Fact]
         public void DadoUmProdutoComValor35DeveRetornarBarato()
         {
-        //Given
-        produto.Preco = 35;
-        Mock<IVerificadorPrecoProduto> mock = new Mock<IVerificadorPrecoProduto>();
-        mock.Setup(m => m.VerificaPrecoProduto(produto)).Returns("Produto Barato!");
-        
-        //When
-        var resultadoEsperado = mock.Object.VerificaPrecoProduto(produto);
-        var resultado = verificador.VerificaPrecoProduto(produto);
+            //Given
+            produto.Preco = 35;
+            Mock<IVerificadorPrecoProduto> mock = new Mock<IVerificadorPrecoProduto>();
+            mock.Setup(m => m.VerificaPrecoProduto(produto)).Returns("Produto Barato!");
+            
+            //When
+            var resultadoEsperado = mock.Object.VerificaPrecoProduto(produto);
+            var resultado = verificador.VerificaPrecoProduto(produto);
 
-        //Then
-        Assert.Equal(resultado, resultadoEsperado);
+            //Then
+            Assert.Equal(resultado, resultadoEsperado);
+        }
+
+        [Fact]
+        public void DadoUmProdutoComValor41DeveRetornarMedio()
+        {
+            //Given
+            produto.Preco = 41;
+            Mock<IVerificadorPrecoProduto> mock = new Mock<IVerificadorPrecoProduto>();
+            mock.Setup(m => m.VerificaPrecoProduto(produto)).Returns("Produto na média de preço!");
+            
+            //When
+            var resultadoEsperado = mock.Object.VerificaPrecoProduto(produto);
+            var resultado = verificador.VerificaPrecoProduto(produto);
+
+            //Then
+            Assert.Equal(resultado, resultadoEsperado);
+        }
+
+        [Fact]
+        public void DadoUmProdutoComValor150DeveRetornarCaro()
+        {
+            //Given
+            produto.Preco = 150;
+            Mock<IVerificadorPrecoProduto> mock = new Mock<IVerificadorPrecoProduto>();
+            mock.Setup(m => m.VerificaPrecoProduto(produto)).Returns("Produto Caro!");
+            
+            //When
+            var resultadoEsperado = mock.Object.VerificaPrecoProduto(produto);
+            var resultado = verificador.VerificaPrecoProduto(produto);
+
+            //Then
+            Assert.Equal(resultado, resultadoEsperado);
         }
     }
 }
